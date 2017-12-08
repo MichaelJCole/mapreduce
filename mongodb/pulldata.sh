498abc1b577d5c5468ef37319bcb41fa1f630727
Michael Cole <michael@bigsmall.io>
2017-12-07 17:27:45 -0700

    MongoDB data

--- mongodb/pulldata.sh
+++ mongodb/pulldata.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Download the data
+wget https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-all-titles-in-ns0.gz
+gunzip -k enwiki-latest-all-titles-in-ns0.gz
+
 # Import the data
 mongoimport -d wikipedia -c titles \
   --type tsv --headerline \
