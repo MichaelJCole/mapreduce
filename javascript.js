630ef5735adf981b9fa3cfe401e0ec420dd5efdf
Michael Cole <michael@bigsmall.io>
2017-12-07 17:00:48 -0700

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// Array.map( cb(item) )
+// Array.reduce( cb(total, item), [initalTotal])
+
+animals = ['cat','dog','rock','fish','snake','turtle'];
+
+animalsMap = animals.map(function(animal) {
+  var strVowels = animal.match(/[aeiouy]/gi);       // string
+  var numVowels = strVowels ? strVowels.length : 0; // number
+  return { first: animal.charAt(0),
+           vowels: numVowels,
+           consonants: animal.length - numVowels };
+});
+
+reduced = animalsMap.reduce(function(total, mappedAnimal) {
+  return total + mappedAnimal.vowels;
+}, 0);  // What is the 0?  What if it was []?
+
+console.log('animals', animals, 'animalsMap', animalsMap, 'reduced', reduced);
