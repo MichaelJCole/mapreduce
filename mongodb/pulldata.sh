a0647d31fdb1d7b3ce99a08dea8a29acd5f010dc
Michael Cole <michael@bigsmall.io>
2017-12-07 20:09:22 -0700

    bugfix

--- mongodb/pulldata.sh
+++ mongodb/pulldata.sh
@@ -7,7 +7,7 @@ gunzip -k enwiki-latest-all-titles-in-ns0.gz
 # Import the data
 mongoimport -d wikipedia -c titles \
   --type tsv --headerline \
-  --file enwiki-20160601-all-titles-in-ns0
+  --file enwiki-latest-all-titles-in-ns0
 
 # Start the mongo console
 mongo
